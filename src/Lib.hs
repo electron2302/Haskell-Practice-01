82985f0cc18591560095ac67cb3b277b966c20cd
electron2302 <technikon23@gmail.com>
2020-03-25 21:59:42 +0100

    Added ggT impl and test

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,7 @@
 module Lib
     ( someFunc
      , square
+     , ggT
     ) where
 
 someFunc :: IO ()
@@ -12,3 +13,10 @@ square :: Num a
           => a -- ^ the number
           -> a -- ^ the square
 square n = n * n
+
+-- | Calculate the ggt of two ints
+ggT :: Integer -> Integer -> Integer
+ggT a b = 
+    if b == 0
+        then a 
+        else ggT b (a `mod` b)
---
+++ test/LibSpec.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module LibSpec (spec) where
+ 
+import Lib (square, ggT)
+import Test.Hspec
+import Test.QuickCheck
+
+spec :: Spec
+spec = do
+  describe "square" $ do
+    it "calculates the square of 5.3" $
+      square 5.3 `shouldBe` 28.09
+    it "calculates the square of an arbitrary integer" $
+      property $ \(n :: Integer) -> square n == n * n
+    it "calculates the square of an arbitrary double" $
+      property $ \(n :: Double) -> square n == n * n
+
+  describe "ggT" $ do
+    it "calculates the ggT of 20 and 30" $
+      ggT 20 30 `shouldBe` 10
+    it "calculates the ggT of arbitrary integers" $
+      property $ \a b -> (a > 0 && b > 0) ==> ggT a b == gcd a b 
+      -- for some reason gcd gets difrent numbers for negative start values like gcd(5,-22)=1 aber euklid(5,-22)=-1
